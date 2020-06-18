7dea040e45d3f0a91d65de2ce19ffeca6ad1b81c
amir-shariati <seyed.amir.shariati@gmail.com>
2020-06-19 00:19:07 +0430

    init scene and camera is added

---
+++ ex1/src/js/main.js
@@ -0,0 +1,26 @@
+
+function init() {
+
+    var scene = new THREE.Scene();
+    // var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+    var camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    // document.body.appendChild( renderer.domElement );
+    document.getElementById('webgl').appendChild(renderer.domElement);
+
+    renderer.render( scene, camera );
+    // renderer.render(
+    //     scene,
+    //     camera
+    // );
+
+}
+
+init();
