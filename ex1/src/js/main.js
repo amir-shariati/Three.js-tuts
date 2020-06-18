528006c73d4f12530c31eaff88fdeb5872aea6d0
amir-shariati <seyed.amir.shariati@gmail.com>
2020-06-19 00:42:04 +0430

    box is added to scene

--- ex1/src/js/main.js
+++ ex1/src/js/main.js
@@ -2,6 +2,11 @@
 function init() {
 
     var scene = new THREE.Scene();
+
+    var box = getBox(1, 1, 1);
+
+    scene.add(box);
+
     // var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
     var camera = new THREE.PerspectiveCamera(
         45,
@@ -9,6 +14,12 @@ function init() {
         1,
         1000
     );
+
+    camera.position.x = 1 ;
+    camera.position.y = 2 ;
+    camera.position.z = 5 ;
+
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,6 +32,19 @@ function init() {
     //     camera
     // );
 
+}
+
+function getBox(width, height, depth) {
+    var geometry = new THREE.BoxGeometry(width, height, depth);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    var mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+
+    return mesh;
 }
 
 init();
